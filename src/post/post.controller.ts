47da79d170b30bdf801d9381f660a1a30a902101
Sahil Bhatt <ethenhunt556@gmail.com>
2024-02-07 15:54:31 +0530

    Create comment route add

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CreatePostDto } from './dto/post/create-post.dto';
 import { PostService } from './post.service';
+import { CreateCommentDto } from './dto/comment/create-comment.dto';
 
 @Controller('post')
 export class PostController {
@@ -20,5 +21,21 @@ export class PostController {
         @Req() req: any
     ){
         return this.postService.createPost(createPostDto, req.user.id, file);
+    }
+
+    @Post('comment')
+    @UseGuards(AuthGuard)
+    comment(
+        @Body() createCommentDto: CreateCommentDto,
+    ){
+        return this.postService.comment(createCommentDto);
+    }
+
+    @Get(':id')
+    @UseGuards(AuthGuard)
+    findOne(
+        @Param('id') id: string
+    ){
+        return this.postService.findOne(id);
     }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { Comment } from './entities/comment.entity'; 
 import { Cloudinary } from 'src/cloudinary/cloudinary';
+import { CreateCommentDto } from './dto/comment/create-comment.dto';
 
 @Injectable()
 export class PostService {
@@ -13,11 +14,29 @@ export class PostService {
         @InjectRepository(Comment) private commentRepository: Repository<Comment>,
         private cloudinary: Cloudinary
     ) { }
+    
+    async findOne(id: string) {
+        return this.postRepository.findOne({
+            where: { id },
+            relations: ['comments', 'comments.madeBy']
+        });
+    }
+
 
     async createPost(createPostDto: any, id: any, file: any) {
         const { url } = await this.cloudinary.uploadFile(file);
         const post = this.postRepository.create({ ...createPostDto, user: id , image: url });
         return this.postRepository.save(post);
+    }
+
+    async comment(commentDto: CreateCommentDto) {
+        const { post, user } = commentDto;
+        const comment = this.commentRepository.create({
+            ...commentDto,
+            post: post,
+            madeBy: user
+        });
+        return this.commentRepository.save(comment);
     }
 
 }
