632454a4ea396d0a5b826a3068c80320ff6c7928
Sahil8383 <sahilbhatt915@gmail.com>
2024-05-20 22:42:47 +0530

    test commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(' From Ubantu 3')
+  console.log(' From Ubantu 4')
   await app.listen(3000);
 }
 bootstrap();
