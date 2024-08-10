d8a8308ed3c15b0374786b152c742b5f94643ae4
Sahil8383 <sahilbhatt915@gmail.com>
2024-08-10 16:59:00 +0530

    from mac

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(' From Ubantu 4')
+  console.log('From Mac')
   await app.listen(3000);
 }
 bootstrap();
